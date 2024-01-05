9e84a4f2841324922ff06a8530b1a5846d8ec108
YashSHIrsath <134185317+YashSHIrsath@users.noreply.github.com>
2024-01-05 11:36:36 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,55 @@
+function seterror(id,error){
+    element1 = document.getElementById(id);
+    element1.getElementsByClassName('formerror')[0].innerHTML = error;
+}
+
+function clearErrors(){
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+}
+
+function validateForm(){
+    var password1 =  document.forms['myForm']['fpassword'].value;
+    
+    const hasCapitalLetter = /[A-Z]/.test(password1);
+    const hasSymbol = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password1);
+    const hasNumber = /[0-9]/.test(password1);
+    var returnVal = true;
+    clearErrors();
+    var name = document.forms['myForm']['fname'].value;
+    if(name.length>15)
+    {
+        seterror('name','*First name length is too long*');
+        returnVal = false;
+    }
+
+    var lname = document.forms['myForm']['flname'].value;
+    if(lname.length>20)
+    {
+        seterror('lname','*Last name length is too long*');
+        returnVal = false;
+    }
+
+    var email = document.forms['myForm']['femail'].value;
+    if(email.length>40)
+    {
+        seterror('email','*Email length is too long*');
+        returnVal = false;
+    }
+
+    var password = document.forms['myForm']['fpassword'].value;
+    if(password.length<6)
+    {
+        seterror('password','*Password is too short(atleast 6 letters)*');
+        returnVal = false;
+    }
+    if (!hasCapitalLetter || !hasSymbol || !hasNumber) {
+        seterror('password','*Password must have atleast 1 symbol ,a Capital letter and a Number.*');
+        console.log(password1)
+        returnVal = false; 
+    }
+    return returnVal;
+}
